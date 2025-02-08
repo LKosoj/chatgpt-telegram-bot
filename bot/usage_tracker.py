dfcea662eb3d6cabf9e10945dfefa5e85a8d01e1
LKosoj <lkosoj@gmail.com>
2025-02-08 03:31:43 +0300

    Update usage_tracker.py

--- bot/usage_tracker.py
+++ bot/usage_tracker.py
@@ -85,10 +85,10 @@ class UsageTracker:
         # update usage_history
         if str(today) in self.usage["usage_history"]["chat_tokens"]:
             # add token usage to existing date
-            self.usage["usage_history"]["chat_tokens"][str(today)] += tokens
+            self.usage["usage_history"]["chat_tokens"][str(today)] = int(self.usage["usage_history"]["chat_tokens"][str(today)]) + int(tokens)
         else:
             # create new entry for current date
-            self.usage["usage_history"]["chat_tokens"][str(today)] = tokens
+            self.usage["usage_history"]["chat_tokens"][str(today)] = int(tokens)
 
         # write updated token usage to user file
         with open(self.user_file, "w") as outfile:
